40675db69a72fcefd3ead5a92219a690821524b3
Felix <dexdaprotocol@gmail.com>
2022-01-23 11:56:09 -0500

    minor update
    minor update in design decisions, put in AVAX logos and changed the HOW TO section . Login component has a few changes as well. including team member names.

--- components/Header.js
+++ components/Header.js
@@ -1,50 +1,49 @@
-import { useMoralis } from "react-moralis"
-import Avatar from "./Avatar"
-import ChangeUsername from "./ChangeUsername"
-// import Image from "next/image"
+import { useMoralis } from "react-moralis";
+import Avatar from "./Avatar";
+import ChangeUsername from "./ChangeUsername";
+import Image from "next/image";
 
 function Header() {
+  const { user, logoutOnPress } = useMoralis();
+  const walletAddress = user.get("ethAddress");
 
-    const { user, logoutOnPress } = useMoralis();
-    const walletAddress = user.get("ethAddress");
+  function copyClipboardAddress() {
+    navigator.clipboard.writeText(walletAddress);
+  }
 
-    function copyClipboardAddress() {
-        navigator.clipboard.writeText(walletAddress)
-    }
-
-     
-    return (
-        <div>
-        <div className="sticky top-0 p-3 z-50 bg-gradient-to-b from-slate-700/[.6] to-teal-800/[.05] shadow-lg text-teal-800 rounded-3xl border-b-2 border-teal-300">
-            <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
-                <div className="relative h-12 w-12 mx-auto hidden lg:inline-grid">
-                    {/* <Image 
-                        layout="fill" 
-                        objectFit="cover"
-                        className="rounded-full" 
-                        src="/FP.png"
-                    />  */}
-                    {/* ADD GAME IMAGE ABOVE */}
-                </div>
-                <div className="col-span-4 text-left lg:text-center">
-                    <div className="relative h-16 w-16 lg:mx-auto border-teal-500 rounded-full border-2 bg-teal-100" >
-                       <Avatar logoutOnPress/>
-                    </div>
-                    <h2 className="flex flex-col text-l text-black font-bold truncate p-1 ">{user.getUsername()}</h2>
-                    <p 
-                    onClick={copyClipboardAddress}
-                    className="flex flex-col text-xs text-black font-light truncate pb-2 hover:cursor-pointer active:text-teal-100">{walletAddress}
-                    </p>
-                    <ChangeUsername />
-                </div>
+  return (
+    <div>
+      <div className="sticky top-0 p-3 z-50 bg-gradient-to-b from-slate-700/[.6] to-teal-800/[.05] shadow-lg text-teal-800 rounded-3xl border-b-2 border-teal-300">
+        <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
+          <div className="relative h-12 w-12 mx-auto hidden lg:inline-grid">
+            <Image
+              height={50}
+              width={50}
+              className="rounded-full"
+              src="/Avax_logo.png"
+            />
+            {/* ADD GAME IMAGE ABOVE */}
+          </div>
+          <div className="col-span-4 text-left lg:text-center">
+            <div className="relative h-16 w-16 lg:mx-auto border-teal-500 rounded-full border-2 bg-teal-100">
+              <Avatar logoutOnPress />
             </div>
-        
-
+            <h2 className="flex flex-col text-l text-black font-bold truncate p-1 ">
+              {user.getUsername()}
+            </h2>
+            <p
+              onClick={copyClipboardAddress}
+              className="flex flex-col text-xs text-black font-light truncate pb-2 hover:cursor-pointer active:text-teal-100"
+            >
+              {walletAddress}
+            </p>
+            <ChangeUsername />
+          </div>
         </div>
-        {/* <Navbar /> */}
-        </div>
-
-    )
+      </div>
+      {/* <Navbar /> */}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,26 @@
-import Head from 'next/head'
-import Login from '../components/Login'
+import Head from "next/head";
+import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
-import Header from '../components/Header';
-import Messages from '../components/Messages';
-
+import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
+  const { isAuthenticated } = useMoralis();
 
-  const { isAuthenticated } = useMoralis(); 
-
-
-  if(!isAuthenticated) return <Login />;
+  if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen overflow-y-scroll scrollbar-hide  bg-gradient-to-b from-slate-500 to-teal-100"> 
+    <div className="h-screen overflow-y-scroll scrollbar-hide  bg-gradient-to-b from-slate-500 to-teal-100">
       <Head>
-        <title>Metaverse Challenge</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>AVAX GAMECENTER</title>
+        <link rel="icon" href="/Avax_logo.png" />
       </Head>
-        <div className='max-w-screen-2xl mx-auto'>
+      <div className="max-w-screen-2xl mx-auto">
         <Header />
         <Messages />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
 
 // overflow-auto scrollbar-hide for head div
